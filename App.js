21865ad8a7368397c696cfe1e9702503a72aa686
ohnas <ohnas12@gmail.com>
2023-06-05 19:05:52 +0900

    Pan Responder part Two

--- App.js
+++ App.js
@@ -39,6 +39,16 @@ export default function App() {
           y: dy,
         });
       },
+      onPanResponderRelease: () => {
+        Animated.spring(POSITION, {
+          toValue: {
+            x: 0,
+            y: 0,
+          },
+          bounciness: 20,
+          useNativeDriver: false,
+        }).start();
+      },
     })
   ).current;
   return (
@@ -48,7 +58,7 @@ export default function App() {
         style={{
           borderRadius,
           backgroundColor: bgColor,
-          transform: [...POSITION.getTranslateTransform()],
+          transform: POSITION.getTranslateTransform(),
         }}
       />
     </Container>
